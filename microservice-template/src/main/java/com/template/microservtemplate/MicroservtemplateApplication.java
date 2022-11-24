06028a4db45197f74f4a1d7e46bef1caeb55b968
Davide De Rosa <davide.derosa@psen.wbaoneit.com>
2022-11-24 22:15:48 +0100

    Structure

---
+++ microservice-template/src/main/java/com/template/microservtemplate/MicroservtemplateApplication.java
@@ -0,0 +1,13 @@
+package com.template.microservtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservtemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservtemplateApplication.class, args);
+    }
+
+}
