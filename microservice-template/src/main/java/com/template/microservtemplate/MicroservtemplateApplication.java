41c83bd5779324b32913b836e21ae40ccf30c8ae
carmine,ferrillo <carmine.ferrillo@psen.wbaoneit.com>
2022-11-25 10:35:53 +0100

    Test

--- microservice-template/src/main/java/com/template/microservtemplate/MicroservtemplateApplication.java
+++ microservice-template/src/main/java/com/template/microservtemplate/MicroservtemplateApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroservtemplateApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(MicroservtemplateApplication.class, args);
     }
 
