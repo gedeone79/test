4f51a3cfa47124361bfe4b02998e4c8f5d9f56e0
carmine,ferrillo <carmine.ferrillo@psen.wbaoneit.com>
2022-11-25 17:05:20 +0100

    pom and package name

--- microservice-template/src/main/java/com/quicktutorials/learnmicroservices/AccountMicroserviceApplication.java
+++ microservice-template/src/main/java/com/quicktutorials/learnmicroservices/AccountMicroserviceApplication.java
@@ -1,10 +1,14 @@
 package com.quicktutorials.learnmicroservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccountMicroserviceApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(AccountMicroserviceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(AccountMicroserviceApplication.class, args);
