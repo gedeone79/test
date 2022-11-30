8d3e2c5bb5ae00eb58639e69a21f96615de9a4c9
Davide De Rosa <davide.derosa@psen.wbaoneit.com>
2022-11-30 23:02:37 +0100

    Structure

--- microservice-template/src/main/java/com/quicktutorials/learnmicroservices/AccountMicroserviceApplication.java
+++ microservice-template/src/main/java/com/quicktutorials/learnmicroservices/AccountMicroserviceApplication.java
@@ -2,11 +2,13 @@ package com.quicktutorials.learnmicroservices;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class AccountMicroserviceApplication {
+public class AccountMicroserviceApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(AccountMicroserviceApplication.class);
 
@@ -14,4 +16,9 @@ public class AccountMicroserviceApplication {
         SpringApplication.run(AccountMicroserviceApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("*** START APPLICATION");
+    }
 }
